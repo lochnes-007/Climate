e7598a4222938c2d00ffc891f20553f6fdba8f62
Ayush-Thasale-007 <codeayushthasale007@gmail.com>
2026-09-05 20:51:53 +0530

    added tests file

---
+++ tests/q1_1_4.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_1_4',
+    'points': [1],
+    'suites': [{'cases': [{'code': '>>> type(coordinates_to_region(50, 100)) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
